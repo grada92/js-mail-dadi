1e7908c823756e2206564440a962553be1a09382
grada92 <danielegradassai@gmail.com>
2022-08-26 14:15:58 +0200

    provo condizione con math.random

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('js ok')
+
+let numberPc = parseInt(Math.random() * 7);
+console.log(numberPc)
+
+let numberUser = parseInt(Math.random() * 7);
+console.log(numberUser)
+
+if (numberUser > numberPc) 
+{
+    numberUser = 'winner'
+    console.log('winner')
+}
+else {
+    console.log('lose')
+
+}
